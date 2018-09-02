c34bc0a2763803338aede079076a83c5edf9b38a
Jimmy Niu <chenenniu@gmail.com>
2018-09-02 02:50:27 -0400

    started clickhandlre for resume

--- main.js
+++ main.js
@@ -1,58 +1,70 @@
-$(document).ready(function () {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini|Mobile/i.test(navigator.userAgent)) {
-        set_up_mobile();
-    } else {
-        create_canvas();
-    }
-    bind_handlers();
+$(document).ready(function() {
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini|Mobile/i.test(
+      navigator.userAgent
+    )
+  ) {
+    set_up_mobile();
+  } else {
+    create_canvas();
+  }
+  bind_handlers();
 });
 
-$(window).on('resize', function () {
-    resize_canvas();
+$(window).on("resize", function() {
+  resize_canvas();
 });
 
 let toDraw = true;
 function bind_handlers() {
-    $("#header, #tagline").hover(function (e) {
-        console.log("in");
-        toDraw = false;
-    }, function (e) {
-        console.log("out");
-        toDraw = true;
-    });
+  $("#header, #tagline").hover(
+    function(e) {
+      console.log("in");
+      toDraw = false;
+    },
+    function(e) {
+      console.log("out");
+      toDraw = true;
+    }
+  );
 
-    $(document).on("mousemove", function (e) {
-        if (toDraw) {
-            light_box(e.clientX, e.clientY);
-        }
-    });
+  $(document).on("mousemove", function(e) {
+    if (toDraw) {
+      light_box(e.clientX, e.clientY);
+    }
+  });
+
+  $("#resume").click(function(e) {
+    console.log("clicked");
+  });
 }
 
 function set_up_mobile() {
-    $("#cards").css("display", "none");
-    $("#mobile_bar").css("display", "flex");
-    create_canvas();
+  $("#cards").css("display", "none");
+  $("#mobile_bar").css("display", "flex");
+  create_canvas();
 }
 
 function checkHash(hash, obj) {
-    return hash[obj] === true;
+  return hash[obj] === true;
 }
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+  return array;
 }
